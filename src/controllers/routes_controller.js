648d74c05474762f9a1ce60d8465b7d8fab0877a
Duarte <rduart3@yahoo.com.br>
2013-11-16 20:00:05 -0200

    Fix #12

--- src/controllers/routes_controller.js
+++ src/controllers/routes_controller.js
@@ -1,9 +1,13 @@
 var app = require('../setup').app;
-
+var Game = require('../setup').Game;
 app.get('/', function(req, res) {
   res.render('index');
 });
 
 app.get('/:id', function(req, res) {
-  res.render('game');
+  Game.find(req.params.id, function(err, result){
+  	if(err)
+  		throw err;
+	res.json(result);
+  });
 });
--- src/setup.js
+++ src/setup.js
@@ -3,6 +3,7 @@ var http = require('http');
 var app = exports.app = require('./config/express');
 var server = http.createServer(app);
 var io = exports.io = require('./config/socketio')(server);
+var Game = exports.Game = require('./model/game');
 
 require('./controllers/routes_controller');
 require('./controllers/sockets_controller');
